3013cd7f53f3ace2e9670b90316c71302fba2d2a
Alex Sampaio <alex.sampaio1991@gmail.com>
2017-02-01 17:30:44 +0100

    // Version updated to v2.0.1

--- statsproduct.php
+++ statsproduct.php
@@ -39,7 +39,7 @@ class statsproduct extends ModuleGraph
     {
         $this->name = 'statsproduct';
         $this->tab = 'analytics_stats';
-        $this->version = '2.0.0';
+        $this->version = '2.0.1';
         $this->author = 'PrestaShop';
         $this->need_instance = 0;
 
